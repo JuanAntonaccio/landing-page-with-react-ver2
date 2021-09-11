9f2118f31a0a24f924b70f4f60208727004fc6bb
JuanAntonaccio <jmantonaccio@gmail.com>
2021-09-11 11:55:29 +0000

    Arreglo en sabado

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
 	return (
 		<div className="text-center bg-dark ">
-			<h2 className="text-white">Copyrigth</h2>
+			<h2 className="text-white">Copyrigth --- by Juan Antonaccio</h2>
 		</div>
 	);
 };
--- src/js/component/tarjetas_varias.js
+++ src/js/component/tarjetas_varias.js
@@ -2,16 +2,8 @@ import React from "react";
 import Tarjeta from "./tarjeta";
 
 const Tarjetas_Varias = () => {
-	let valores={
-		tarjeta:"tarjeta 1",
-		tarjeta:"tarjeta 2",
-		tarjeta:"tarjeta 3",
-		tarjeta:"tarjeta 4"
-	}
-
 	return (
 		<div className="row">
-			
 			<div className="col">
 				<Tarjeta />
 			</div>
